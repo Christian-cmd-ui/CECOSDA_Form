94cf8114c3d0d323f9d6386db8613c8ee7b58b0d
Christian Daniel <danbel418@gmail.com>
2024-02-26 12:59:43 +0100

    first commit

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function aboutAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/about.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/projects", name="projects")
+     */
+    public function projectsAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/projects.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/contact.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/services", name="services")
+     */
+    public function servicesAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/services.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function blogAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/blog.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/formation-assistanat", name="formation-assistanat")
+     */
+    public function assistanatAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/formation-assistanat.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+    
+}
